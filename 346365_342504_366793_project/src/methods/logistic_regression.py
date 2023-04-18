ffe9ea4e0833452ed5a1206b8c1a3b33bb3d77a0
Noé Macé <nm@tsf-452-wpa-1-078.epfl.ch>
2023-04-18 11:35:25 +0200

    Added logistic regression model

--- 346365_342504_366793_project/src/methods/logistic_regression.py
+++ 346365_342504_366793_project/src/methods/logistic_regression.py
@@ -5,10 +5,10 @@ from ..utils import get_n_classes, label_to_onehot, onehot_to_label
 
 class LogisticRegression(object):
     """
-    Logistic regression classifier.
+    Logistic regression classifier with bias term.
     """
 
-    def __init__(self, lr, max_iters=500):
+    def __init__(self, lr, max_iters=500, reg_lambda=0.01, eps=1e-8, gamma=1e-8):
         """
         Initialize the new object (see dummy_methods.py)
         and set its arguments.
@@ -16,39 +16,65 @@ class LogisticRegression(object):
         Arguments:
             lr (float): learning rate of the gradient descent
             max_iters (int): maximum number of iterations
+            reg_lambda (float): regularization strength
+            eps (float) : small constant to prevent division by zero, log(0) 
+            gamma (float) : stopping the gradient descent
         """
         self.lr = lr
         self.max_iters = max_iters
+        self.reg_lambda = reg_lambda
+        self.eps = eps
+        self.gamma = gamma
+
+    def f_softmax(self, data, W, b):
+        # Softmax function with bias term
+
+        scores = np.dot(data, W) + b
+        exp_scores = np.exp(scores - np.max(scores, axis=1, keepdims=True))
+
+        probs = exp_scores / \
+            (np.sum(exp_scores, axis=1, keepdims=True) + self.eps)
+        return probs
+
+    def loss_logistic_multi(self, data, labels, w, b):
+        y = self.f_softmax(data, w, b)
+        loss = -np.sum(labels * np.log(y + self.eps)) / data.shape[0]
+        # reg_loss = 0.5 * self.reg_lambda * np.sum(w**2)
+        return loss  # + reg_loss
+
+    def gradient_logistic_multi(self, data, labels, W, b):
+        return np.dot(np.transpose(data), (self.f_softmax(data, W, b) - labels)), np.sum(self.f_softmax(data, W, b) - labels, axis=0)
 
     def fit(self, training_data, training_labels):
-        """
-        Trains the model, returns predicted labels for training data.
+        labels = label_to_onehot(training_labels)
 
-        Arguments:
-            training_data (array): training data of shape (N,D)
-            training_labels (array): regression target of shape (N,)
-        Returns:
-            pred_labels (array): target of shape (N,)
-        """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE! 
-        ###
-        ##
+        N, D = training_data.shape
+        C = get_n_classes(training_labels)
+
+        self.weights = np.random.normal(0, 0.1, (D, C))
+        self.bias = np.zeros(C)
+
+        prev_loss = np.inf
+        for it in range(self.max_iters):
+            gradient_w, gradient_b = self.gradient_logistic_multi(
+                training_data, labels, self.weights, self.bias)
+            self.weights = self.weights - self.lr * \
+                (gradient_w + self.reg_lambda * self.weights)
+            self.bias = self.bias - self.lr * gradient_b
+            current_loss = self.loss_logistic_multi(
+                training_data, labels, self.weights, self.bias)
+
+            # Early stopping condition
+            if np.abs(prev_loss - current_loss) < self.gamma:
+                break
+            prev_loss = current_loss
+        predictions = self.predict(training_data)
+
         return self.predict(training_data)
 
     def predict(self, test_data):
-        """
-        Runs prediction on the test data.
-        
-        Arguments:
-            test_data (array): test data of shape (N,D)
-        Returns:
-            pred_labels (array): labels of shape (N,)
-        """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE! 
-        ###
-        ##
+        prob = self.f_softmax(test_data, self.weights, self.bias)
+        pred_labels = np.zeros(test_data.shape[0])
+        for i in range(pred_labels.shape[0]):
+            pred_labels[i] = np.argmax(prob[i])
         return pred_labels
